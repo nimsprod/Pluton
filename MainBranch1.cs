0ce33c852b30573eff156ba3238b15632db8f809
Jose Saez <jsaez@foreworth.com>
2021-01-01 10:51:07 +0100

    initial commit

---
+++ MainBranch1.cs
@@ -0,0 +1,17 @@
+namespace PlutonApplication
+{
+    public class Planet
+    {
+        public void main()
+        {
+            Console.WriteLine("Hola");			
+			Console.WriteLine("Hola Mundo");	
+			Console.WriteLine("Que tal?");	
+			Console.WriteLine("Bien");
+			Console.WriteLine("A tope");
+			Console.WriteLine("Excelente");
+			Console.WriteLine("Fin");
+			Console.WriteLine("Tobe");
+        }
+    }
+}

0220eff2a9c1891cbde96696eb0ad89fcfea331d
Jose Saez <jsaez@foreworth.com>
2021-04-01 10:51:07 +0200

    commit b

---
+++ Second.cs
@@ -0,0 +1,16 @@
+namespace PlutonApplication
+{
+    public class Interesting
+    {
+        public void main()
+        {
+            Console.WriteLine("Hola");			
+			Console.WriteLine("Hola Mundo");	
+			Console.WriteLine("Que tal?");	
+			Console.WriteLine("Bien");
+			Console.WriteLine("A tope");
+			Console.WriteLine("Excelente");
+			Console.WriteLine("Fin");
+        }
+    }
+}
